11cd842854f3bee42b6cf6172f9ce3ff453448d2
hamid <purhasanih@gmail.com>
2022-06-28 01:43:42 +0430

    test base

---
+++ pages/tests.py
@@ -0,0 +1,27 @@
+# pages/tests.py
+from django.test import SimpleTestCase
+from django.urls import reverse
+from .views import HomePageView
+
+
+class HomepageTests(SimpleTestCase):
+    
+    def test_homepage_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_homepage_url_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_homepage_template(self): # new
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_homepage_contains_correct_html(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'Homepage')
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        response = self.client.get('/')
+        self.assertNotContains(response, 'Hi there! I should not be on the page.')
